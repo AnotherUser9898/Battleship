0afebb084c6acde5b980ab42682ee81ed04956f0
AnotherUser9898 <anotheruser9898@gmail.com>
2024-08-28 12:07:04 +0530

    Initial dist subtree commit

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkbattleship"] = self["webpackChunkbattleship"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("Message");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmF0dGxlc2hpcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIk1lc3NhZ2VcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
